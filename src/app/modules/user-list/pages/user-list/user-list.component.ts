fdea41f088d49b403853696b7ce0d6c29a106643
Chris Gabriel Costelo <ccostelo@77global.biz>
2023-06-05 18:07:25 +0800

    Update: Dashboard, Products, Users template

--- src/app/modules/user-list/pages/user-list/user-list.component.ts
+++ src/app/modules/user-list/pages/user-list/user-list.component.ts
@@ -1,14 +1,164 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable, map, startWith } from 'rxjs';
+
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  middleName?: string;
+  email: string;
+  birthdate: Date;
+  username: string;
+  password: string;
+  listOfInterest: string[];
+  role: string;
+}
+
+const ELEMENT_DATA: User[] = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: 'Kent',
+    email: 'johndoe@email.com',
+    birthdate: new Date('06-05-2023'),
+    username: 'johndoe',
+    password: 'pass123',
+    listOfInterest: ['Desktop','Computer Components', 'Accessories'],
+    role: 'User'
+  },
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: 'Kent',
+    email: 'johndoe@email.com',
+    birthdate: new Date('06-05-2023'),
+    username: 'johndoe',
+    password: 'pass123',
+    listOfInterest: ['Desktop','Computer Components', 'Accessories'],
+    role: 'User'
+  },
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: 'Kent',
+    email: 'johndoe@email.com',
+    birthdate: new Date('06-05-2023'),
+    username: 'johndoe',
+    password: 'pass123',
+    listOfInterest: ['Desktop','Computer Components', 'Accessories'],
+    role: 'User'
+  },
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: 'Kent',
+    email: 'johndoe@email.com',
+    birthdate: new Date('06-05-2023'),
+    username: 'johndoe',
+    password: 'pass123',
+    listOfInterest: ['Desktop','Computer Components', 'Accessories'],
+    role: 'User'
+  },
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: 'Kent',
+    email: 'johndoe@email.com',
+    birthdate: new Date('06-05-2023'),
+    username: 'johndoe',
+    password: 'pass123',
+    listOfInterest: ['Desktop','Computer Components', 'Accessories'],
+    role: 'User'
+  },
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: 'Kent',
+    email: 'johndoe@email.com',
+    birthdate: new Date('06-05-2023'),
+    username: 'johndoe',
+    password: 'pass123',
+    listOfInterest: ['Desktop','Computer Components', 'Accessories'],
+    role: 'User'
+  },
+]
+
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent {
+export class UserListComponent implements AfterViewInit{
   isShowMenu: boolean = false;
+  interestCtrl = new FormControl('');
+  filteredInterests: Observable<string[]>;
+  separatorKeysCodes: number[] = [13, 188];
+  interests: string[] = [];
+  allInterests: string[] = ['Desktop PC', 'Notebooks', 'Computer Components', 'Computer Peripherals', 'Accessories'];
+
+  @ViewChild('interestInput') interestInput!: ElementRef<HTMLInputElement>;
 
   toggleMenu() {
     this.isShowMenu = !this.isShowMenu;
   }
+
+  constructor(){
+    this.filteredInterests = this.interestCtrl.valueChanges.pipe(
+      startWith(null),
+      map((interest: string | null) =>
+        interest ? this._filter(interest) : this.allInterests.slice()
+      )
+    );
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value && !this.interests.includes(value)) {
+      this.interests.push(value);
+    }
+    event.chipInput!.clear();
+  
+    this.interestCtrl.setValue(null);
+  }
+
+  remove(interest: string): void {
+    const index = this.interests.indexOf(interest);
+    if (index >= 0) {
+      this.interests.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    const value = event.option.viewValue;
+    if (value && !this.interests.includes(value)) {
+      this.interests.push(value);
+    }
+    this.interestInput.nativeElement.value = '';
+    this.interestCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allInterests.filter((interest) =>
+      interest.toLowerCase().includes(filterValue)
+    );
+  }
+
+
+  dataSource = new MatTableDataSource<User>(ELEMENT_DATA);
+  displayedColumns: string[] = ['firstName', 'lastName', 'middleName', 'birthdate', 'username', 'listOfInterest', 'role', 'actions'];
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  
 }
--- src/app/modules/user-list/user-list.module.ts
+++ src/app/modules/user-list/user-list.module.ts
@@ -4,9 +4,19 @@ import { RouterModule } from '@angular/router';
 import { UserListRoutingModule } from './user-list-routing.module';
 import { MatIconModule } from '@angular/material/icon';
 import { UserListComponent } from './pages/user-list/user-list.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTable, MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule, MatIconModule, RouterModule, UserListRoutingModule],
+  imports: [CommonModule, MatIconModule, RouterModule, 
+            UserListRoutingModule, MatAutocompleteModule, 
+            MatChipsModule, MatFormFieldModule, MatInputModule,
+            ReactiveFormsModule, MatTableModule, MatPaginatorModule],
 })
 export class UserListModule {}
